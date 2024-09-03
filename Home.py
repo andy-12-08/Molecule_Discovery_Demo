934ff871b84095c3b44f05f42900a808b3117bd3
Andrew Okafor <aokafo3@lsu.edu>
2024-09-03 15:28:35 -0500

    now

--- Home.py
+++ Home.py
@@ -7,7 +7,10 @@ st.set_page_config(page_title="Molecule Design and Discovery",
                    initial_sidebar_state="expanded",
                    )
 
-st.title("Welcome to Romagnoli's Group")
+st.title("SingletonAI")
+#st.write("Welcome to SingletonAI, a platform for molecule design and discovery using generative AI. This platform is designed to help you generate novel molecules with desired properties. You can use the tools provided here to generate molecules, visualize them, and analyze their properties. You can also use the platform to train your own models and generate molecules using your own data.")
+
+st.markdown("<h5 style='color:blue'> Developed and Deployed by Andrew Okafor <h5>", unsafe_allow_html=True)
 # add a header
 st.header('Demonstration of Molecule Discovery Using Generative AI')
 
