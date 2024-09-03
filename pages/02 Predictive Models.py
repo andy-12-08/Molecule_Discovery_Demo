cf08a86806136453b0498f6208f8e82c3aa90bc2
Andrew Okafor <aokafo3@lsu.edu>
2024-09-03 00:56:45 -0500

    new

--- pages/02 Predictive Models.py
+++ pages/02 Predictive Models.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+from src.pretrainedmodel import GNNmodel
+
+graph_model_path = 'models'
 
 st.title('Predictive Models')
 st.write('Predictive models are used to predict the properties of molecules based on their structure. These models can be broadly classified into two categories:')
@@ -10,3 +14,15 @@ with tab1:
 
 with tab2:
     st.write('Models that leverage molecular graphs for advanced property prediction.')
+    smiles_input = st.text_input('Enter a molecule SMILES')
+    predict_button = st.button('Predict')
+    if predict_button:
+        prediction_model = GNNmodel(path = graph_model_path)
+        smiles_list = [smiles.strip() for smiles in smiles_input.split(',') if smiles.strip()]
+        property_values = []
+        for i in range(len(smiles_list)):
+            pred_value = prediction_model.predict([smiles_list[i]])
+            property_values.append(pred_value[0])
+        property_values = [round(value, 2) for value in property_values]
+        prediction_df = pd.DataFrame({'SMILES': smiles_list, 'Predicted Property Value': property_values})
+        st.write(prediction_df)
